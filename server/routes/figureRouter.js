70171c717ab596bbceacfd69512e3e57bf4758a9
Niyaz Latypov <hidouchiha@gmail.com>
2024-04-07 18:07:45 +0500

    feat: created JWT authorization

--- server/routes/figureRouter.js
+++ server/routes/figureRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const router = new Router();
 const figureController = require('../controllers/figureController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
-router.post('/', figureController.create);
+router.post('/', checkRole('admin'), figureController.create);
 router.get('/', figureController.getAll);
 router.get('/:id', figureController.getOne);
 
